aeebef4d31433ac73d5b0c8a28fc796057788a28
Gokul Rajasekhar <gokul.rajasekhar@emvigotech.com>
2021-01-16 19:55:57 +0530

    5sec

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,4 +1,4 @@
-const { Op } = require('sequelize')
+const { Op, DatabaseError } = require('sequelize')
 const {subtractSecondsFromCurrentTime} = require('../utils');
 const Analytics = require('../models/analytics');
 // Function subtractSecondsFromCurrentTime is a utility function which accepts the seconds to subtract from thee current time and
@@ -6,7 +6,12 @@ const Analytics = require('../models/analytics');
 module.exports.postAnalytics = async(req, res) =>{
     try {
     console.log(req.body);
-    const insertObj= { eventType: req.body.eventType, user: req.body.user, date: req.body.date };
+    const insertObj= { eventType: req.body.eventType, user: req.body.user, date: req.body.date ? req.body.date : new Date() };
+    
+    const isExist = await Analytics.count({ where:{ date: { [Op.gte] : 5}, user: insertObj.user, eventType: insertObj.eventType } })
+    console.log("is" , isExist)
+    //if(isExist > 0)
+        //return res.status(401).send({"message" :"Same request with 5sec reject"});
     const result = await Analytics.create(insertObj);
     console.log(result.id);
     res.status(201).send({"ingested" : result.id});
