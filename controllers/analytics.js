6a9820f975889dc813d63da08a6dbaea02dd4d49
Gokul Rajasekhar <gokul.rajasekhar@emvigotech.com>
2021-01-16 19:29:47 +0530

    Post request with DB insert

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,4 +1,16 @@
 const { Op } = require('sequelize')
 const {subtractSecondsFromCurrentTime} = require('../utils');
+const Analytics = require('../models/analytics');
 // Function subtractSecondsFromCurrentTime is a utility function which accepts the seconds to subtract from thee current time and
 // returns the javascript date object
+module.exports.postAnalytics = async(req, res) =>{
+    try {
+    console.log(req.body);
+    const insertObj= { eventType: req.body.eventType, user: req.body.user, date: req.body.date };
+    const result = await Analytics.create(insertObj);
+    console.log(result.id);
+    res.send({"ingested" : result.id});
+    } catch (error) {
+        res.status(400).send({"message" :"Some error occurred"});
+    }
+}
--- routes/analytics.js
+++ routes/analytics.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const controller = require('../controllers/analytics');
 
+router.post('/', controller.postAnalytics)
 
 module.exports = router;
+
