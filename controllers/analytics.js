24d94fb4324e71fa37bcee37e6f1c12e00d5f3d2
Gokul Rajasekhar <gokul.rajasekhar@emvigotech.com>
2021-01-16 19:57:32 +0530

    final

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -7,11 +7,12 @@ module.exports.postAnalytics = async(req, res) =>{
     try {
     console.log(req.body);
     const insertObj= { eventType: req.body.eventType, user: req.body.user, date: req.body.date ? req.body.date : new Date() };
-    
-    const isExist = await Analytics.count({ where:{ date: { [Op.gte] : 5}, user: insertObj.user, eventType: insertObj.eventType } })
+    var t = new Date();
+    t.setSeconds(t.getSeconds() - 5);
+    const isExist = await Analytics.count({ where:{ date: { [Op.gte] : t}, user: insertObj.user, eventType: insertObj.eventType } })
     console.log("is" , isExist)
-    //if(isExist > 0)
-        //return res.status(401).send({"message" :"Same request with 5sec reject"});
+    if(isExist > 0)
+        return res.status(401).send({"message" :"Same request with 5sec reject"});
     const result = await Analytics.create(insertObj);
     console.log(result.id);
     res.status(201).send({"ingested" : result.id});
