c1a1a3a6646317f1851d58b2e3ba8ad65d4beb49
Gokul Rajasekhar <gokul.rajasekhar@emvigotech.com>
2021-02-14 19:57:43 +0530

    Unit testing basic

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -6,14 +6,28 @@ const Analytics = require('../models/analytics');
 module.exports.postAnalytics = async(req, res) =>{
     try {
     console.log(req.body);
-    const insertObj= { eventType: req.body.eventType, user: req.body.user, date: req.body.date ? req.body.date : new Date() };
-    var t = new Date();
-    t.setSeconds(t.getSeconds() - 5);
-    const isExist = await Analytics.count({ where:{ date: { [Op.gte] : t}, user: insertObj.user, eventType: insertObj.eventType } })
-    console.log("is" , isExist)
-    if(isExist > 0)
-        return res.status(401).send({"message" :"Same request with 5sec reject"});
-    const result = await Analytics.create(insertObj);
+    const data = req.body;
+    let result = '';
+    for(let item of  data){
+        console.log(item)
+        item = {...item, date: new Date() }
+        console.log(item)
+        var t = new Date();
+         t.setSeconds(t.getSeconds() - 2);
+        let isExist = await Analytics.count({ where:{ date: { [Op.gte] : t}, user: item.user, eventType: item.eventType } })
+        console.log("is" , isExist)
+        if(isExist > 0)
+            continue;
+        result = await Analytics.create(item);
+    }
+    // const insertObj= { eventType: req.body.eventType, user: req.body.user, date: req.body.date ? req.body.date : new Date() };
+    // var t = new Date();
+    // t.setSeconds(t.getSeconds() - 5);
+    // const isExist = await Analytics.count({ where:{ date: { [Op.gte] : t}, user: insertObj.user, eventType: insertObj.eventType } })
+    // console.log("is" , isExist)
+    // if(isExist > 0)
+    //     return res.status(401).send({"message" :"Same request with 5sec reject"});
+    // const result = await Analytics.create(insertObj);
     console.log(result.id);
     res.status(201).send({"ingested" : result.id});
     } catch (error) {
